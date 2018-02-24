9c35cb2564c0026f39fa9a4ac6d06b54b99de090
mylilpumpernickel <36419544+mylilpumpernickel@users.noreply.github.com>
2018-02-23 23:27:51 -0500

    Update Main.java

--- Wang-clibook/src/com/company/Main.java
+++ Wang-clibook/src/com/company/Main.java
@@ -1,9 +1,8 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*; // accesses entire java.util library
 
 public class Main {
-
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -46,12 +45,61 @@ public class Main {
 
                     //System.out.println(name[0] + studentNum[0]);
             }
-
             if (choice2.equals("Edit")){
 
             }
-
-
         }
     }
 }
+
+
+// Martin's code below
+/*
+import java.util.*;
+
+
+public class Main {   // This  function stores values onto a list which would later be displayed 
+  public static void main(String[] args) {
+    // Inputting student's information
+
+    Scanner student = new Scanner(System.in);
+    System.out.print("What is this student's name?\t");
+    String studentName = student.next();
+    
+    System.out.print("Please enter " + studentName + "'s Student Number:\t");
+    int studentno = Student.nextInt();
+    
+    System.out.print("What is " + studentName + "'s avaerage mark as of now?\t");
+    boolean col4 = false
+    double studentAvg = student.nextDouble();
+    if (studentAvg <= 65) {
+      boolean col4 = true
+    }
+    
+    System.out.print("Is " + studentName + "missing any assignments (YES/NO)?\t");
+    String assignments = student.next();
+    String missing = assignments.toLowerCase();
+    if (missing.equals("y") || missing.equals("yes") {
+      boolean col4 = true
+    }
+    
+
+    // Printing Class List
+    System.out.print("Do you want to view your Class List (YES/NO)?\t");
+    Scanner maybe = new Scanner(System.in);
+    String view = maybe.next();
+    String classInfo = view.toLowerCase();
+    
+    if (classInfo.equals("y") || classInfo.equals("yes")) {
+      //  Make ASCII tables @ https://ozh.github.io/ascii-tables/
+      System.out.println("╔═════════════════════════════╦═══════════╦═══════════════════════╦════════════════════════════════╗");
+      System.out.println("║        Student Name         ║ Student # ║ Student's Average (%) ║ In Danger?/Missed Assignments? ║");
+      System.out.println("╠═════════════════════════════╬═══════════╬═══════════════════════╬════════════════════════════════╣");
+    } else {
+        System.out.print("Class List not shown.");
+    }
+
+  }
+}
+
+*/
