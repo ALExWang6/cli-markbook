d85d5854ba572595f0f3e963368e8a40025baa1d
mylilpumpernickel <36419544+mylilpumpernickel@users.noreply.github.com>
2018-02-26 22:01:38 -0500

    Update Main.java

--- Wang-clibook/src/com/company/Main.java
+++ Wang-clibook/src/com/company/Main.java
@@ -1,71 +1,80 @@
 import java.util.*;
 
 public class Main {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int choice;
+    int[] studentNum;
+    double[] sAverage;
+    int cAverage;
+    String[] name;
+    int total = 0;
+    int classTotal = 0;
+    String[] assignments;
 
-        Scanner input = new Scanner(System.in);
-        String choice1;
-        int[] studentNum;
-        double[] sAverage;
-        double cAverage;
-        String[] name;
-        int total = 0;
-        boolean[] danger;
-        double classTotal = 0;
-    
+    System.out.print("Please type in the total number of students:");
+    total = input.nextInt();
+    name = new String[total];
+    sAverage = new double[total];
+    studentNum = new int[total];
+    assignments = new String[total];
 
-        System.out.print("Please type in the total number of students.");
-        total = input.nextInt();
-        name = new String[total];
-        sAverage = new double[total];
-        studentNum = new int[total];
-        danger = new boolean[total];
+    System.out.printf("What would you like to view?\n(1)\tStudent #\n(2)\tStudent's Averages\nPlease enter either '1' or '2':\t");
+    choice = input.nextInt();
 
-        System.out.printf("What would you like to do?\nEdit Student info(Edit)? See the list?(list) Check student login information(check)?");
+    //  User wants to view Student #
+    if (choice == 1) {
+      for(int i = 0; i < total; i++) {
 
-        choice1 = input.next();
-
-        if (choice1.equals("Edit")){
-
-            System.out.println("Type in student info.");
+        System.out.print("What is Student #" + (i + 1) + "'s name? ");
+        name[i] = input.next();
+        System.out.print("What is " + name[i] + "'s student number? ");
+        studentNum[i] = input.nextInt();
+      }
         
-
-                    for(int i = 0; i < total; i++) {
-
-                        System.out.print("Enter " + (i+1) + "#student name: ");
-                        name[i] = input.next();
-                        System.out.print("Enter the student's number: ");
-                        studentNum[i] = input.nextInt();
-                        System.out.print("Enter student average: ");
-                        sAverage[i] = input.nextDouble();
-                        if(sAverage[i] <= 65){ 
-                        danger[i] = true;
-                        }
-                        
-                        else{
-                          danger[i] = false;
-                        }
-                        
-                        classTotal += sAverage[i];
-                        
-                        System.out.println("");
-
-                    }
-                    
-                  
-                    cAverage = classTotal / total;
-
-            System.out.println("╔═════════════════════════════╦═══════════╦═══════════════════════╦════════════════════════════════╗");
-            System.out.println("║        Student Name         ║ Student # ║ Student's Average (%) ║ In Danger?/Missed Assignments? ║");
-            System.out.println("╠═════════════════════════════╬═══════════╬═══════════════════════╬════════════════════════════════╣");
+      String classTable1 = "║ %-15s ║ %-9d ║%n";
+      System.out.format("╔═════════════════╦═══════════╗%n");
+      System.out.format("║ Student Name    ║ Student # ║%n");
+      System.out.format("╠═════════════════╬═══════════╣%n");
+      for (int j = 0; j < total; j++) {
+        System.out.format(classTable1, name[j], studentNum[j]);
+      }
+      System.out.format("╚═════════════════╩═══════════╝%n");
+    
+    //  User wants to view Student's averages  
+    } else if (choice == 2) {
+        for(int i = 0; i < total; i++) {
+  
+          System.out.printf("What is Student #" + (i + 1) + "'s name? ");
+          name[i] = input.next();
+          System.out.print("What is " + name[i] + "'s average (%)? ");
+          sAverage[i] = input.nextDouble();
+          System.out.print("Is " + name[i] + " missing any assignments (YES/NO)? ");
+          assignments[i] = input.next().toLowerCase();
           
-          for(int j = 0; j < total; j++){
-            System.out.println("║        " + name[j] + "      " + studentNum[j] + "    " + sAverage[j] + "     " + danger[j] + "║");
+          if (sAverage[i] <= 65 || assignments[i].equals("yes")) {
+            System.out.println(name[i] + "'s grades are in danger!");
           }
+          // Gather sum of averages
+          classTotal += sAverage[i];
+        }
           
-          System.out.println("║   class average: " + cAverage );
-
+        String classTable2 = "║ %-15s ║ %-11g ║%n";
+        System.out.format("╔═════════════════╦═════════════╗%n");
+        System.out.format("║ Student Name    ║ Average (%%) ║%n");
+        System.out.format("╠═════════════════╬═════════════╣%n");
+        for (int j = 0; j < total; j++) {
+          System.out.format(classTable2, name[j], sAverage[j]);
+        }
+        System.out.format("╚═════════════════╩═════════════╝%n");
+        
+        cAverage = classTotal / total;
+        System.out.println("Class Average:\t" + cAverage + "%");
+    
+    //  User enters a number not 1 or 2  
+    } else {
+      System.out.println("Not a valid option! Please restart the program and try again!");
     }
-}
+  }
 }
